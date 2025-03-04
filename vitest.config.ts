3b4c90f8178fc02877e59192d63834ca701e1d7d
Enis Halimi <enis.halimi@trackforce.com>
2025-03-04 11:55:19 +0100

    CI: Adding coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import Vue from "@vitejs/plugin-vue"
 export default defineConfig({
   plugins: [Vue()],
   test: {
-    environment: "happy-dom"
+    environment: "happy-dom",
+    coverage: {
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    }
   }
 })
